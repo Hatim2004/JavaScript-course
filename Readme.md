fc6f9d326ac6bc5707df4bc50443beff7d64b815
Hatim2004 <sqrhatm064@gamil.com>
2025-06-26 23:03:07 +0300

    first

---
+++ Readme.md
@@ -0,0 +1,5 @@
+what I've leard:
+-Number functions like isIntger, isNaN, isFloat.
+-Math functions sqrt, pow, round, abs, ceil, floor.
+-Math values E, PI. 
+-Min and Max functions.
---
+++ script.js
@@ -0,0 +1,14 @@
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
+
+console.log(Number.isInteger(1.2))
+console.log(Number.isNaN("hi"-2))
+ 
+console.log(Math.pow(2,2));
+console.log(Math.PI);
+function calculate(){
+    let input = Number(document.getElementById("input").value);
+    let output = document.getElementById("output");
+    output.innerHTML = input * 0.025 + " ريال";
+}
